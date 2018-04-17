32bfc1bdad43eaee4afa51170c74d01b6656b885
Egor <tka4enkoegor@gmail.com>
2018-04-17 10:22:17 +0300

    Fix qoutes and condition

--- lesson1/task1.rb
+++ lesson1/task1.rb
@@ -1,10 +1,12 @@
-puts "Enter your name: "
+puts 'Enter your name: '
 name = gets.chomp
-puts "Enter your height: "
+puts 'Enter your height: '
 height = gets.chomp.to_i
-  ideal_weight = height - 110
-if ideal_weight < 0
-  puts "Your weight is awesome!"
+
+ideal_weight = height - 110
+
+if ideal_weight.negative?
+  puts 'Your weight is awesome!'
 else
   puts "#{name}, ideal weight is: #{ideal_weight}"
 end
